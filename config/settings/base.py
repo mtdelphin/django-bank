bccc635520e3ac22da6574c975848d9185e7d0a9
Mpo Frank <mpotdelphin@gmail.com>
2025-09-03 14:15:59 +0100

    feat: add django logging with loguru

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv, path
+from loguru import logger
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
-APPS_DIR = BASE_DIR / "core_apps"
+APPS_DIR = BASE_DIR / 'core_apps'
 
-local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+local_env_file = path.join(BASE_DIR, '.envs', '.env.local')
 
 if path.isfile(local_env_file):
     load_dotenv(local_env_file)
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv("POSTGRES_DB"),
+        'USER': getenv("POSTGRES_USER"),
+        'PASSWORD': getenv("POSTGRES_PASSWORD"),
+        'PORT': getenv("POSTGRES_PORT"),
+        'HOST': getenv("POSTGRES_HOST"),
     }
 }
 
@@ -140,3 +145,46 @@ STATIC_ROOT = str(BASE_DIR / 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING_CONFIG = None
+LOGURU_LOGGING = {
+    "handlers": [
+        {
+            "sink": BASE_DIR / "logs/debug.log",
+            "level": "DEBUG",
+            "filter": lambda record: record['level'].no <= logger.level('WARNING').no,
+            "format": "{time:YYYY-MM-DD HH:mm;ss.SSS} | {level: <8} | "
+            "{name}:{function}:{line} - {message}",
+            "rotation": "10MB",
+            "retention": "30 days",
+            "compression": "zip"
+        },
+        {
+            "sink": BASE_DIR / "logs/error.log",
+            "level": "ERROR",
+            "format": "{time:YYYY-MM-DD HH:mm;ss.SSS} | {level: <8} | "
+            "{name}:{function}:{line} - {message}",
+            "rotation": "10MB",
+            "retention": "30 days",
+            "compression": "zip",
+            "backtrace": True,
+            "diagnose": True
+        },
+    ]
+}
+
+
+logger.configure(**LOGURU_LOGGING)
+LOGGING={
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "loguru": {
+            "class": "interceptor.InterceptHandler",
+            "root": {
+                "handlers": ["loguru"],
+                "level": "DEBUG",
+            }
+        }
+    }
+}
