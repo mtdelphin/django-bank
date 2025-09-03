577c00dfe1e5f14ffd12f82cae8592a0b71937eb
Mpo Frank <mpotdelphin@gmail.com>
2025-09-03 18:16:57 +0100

    feat: setup api, postgres and mailpit containters

--- config/settings/base.py
+++ config/settings/base.py
@@ -139,7 +139,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'static')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
