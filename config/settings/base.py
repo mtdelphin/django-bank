726e287962a1c281bd412a946ad774776f4f9fcf
Mpo Frank <mpotdelphin@gmail.com>
2025-09-19 12:42:41 +0100

    feat: add common models and configure cloudinary

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from os import getenv, path
 from dotenv import load_dotenv
 from loguru import logger
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
@@ -179,6 +180,15 @@ CELERY_TASK_SOFT_TIME_LIMIT=60
 CELERY_BEAT_SCHEDULER="django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_TASK_EVENTS=True
 
+CLOUDINARY_API_KEY=getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET=getenv("CLOUDINARY_API_SECRET")
+CLOUDINARY_CLOUD_NAME=getenv("CLOUDINARY_CLOUD_NAME")
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+)
 
 LOGGING_CONFIG = None
 LOGURU_LOGGING = {
