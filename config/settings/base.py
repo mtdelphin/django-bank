e41e12417604732bc8cef1a57875336cdb9c8bfa
Mpo Frank <mpotdelphin@gmail.com>
2025-09-21 00:19:06 +0100

    feat: add custom auth with djoser and simplejwt

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,91 +8,90 @@ import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
-APPS_DIR = BASE_DIR / 'core_apps'
+APPS_DIR = BASE_DIR / "core_apps"
 
-local_env_file = path.join(BASE_DIR, '.envs', '.env.local')
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
 
 if path.isfile(local_env_file):
     load_dotenv(local_env_file)
 
 
-
 # Application definition
 
 DJANGO_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django.contrib.humanize'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.humanize",
 ]
 
 THIRD_PARTY_APPS = [
-    'rest_framework',
-    'django_countries',
-    'phonenumber_field',
-    'drf_spectacular',
-    'djoser',
-    'cloudinary',
-    'django_filters',
-    'djcelery_email',
-    'django_celery_beat',
+    "rest_framework",
+    "django_countries",
+    "phonenumber_field",
+    "drf_spectacular",
+    "djoser",
+    "cloudinary",
+    "django_filters",
+    "djcelery_email",
+    "django_celery_beat",
 ]
 
 LOCAL_APPS = [
-    'core_apps.common',
-    'core_apps.user_auth',
-    'core_apps.user_profile',
+    "core_apps.common",
+    "core_apps.user_auth",
+    "core_apps.user_profile",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core_apps.user_auth.middleware.CustomHeaderMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core_apps.user_auth.middleware.CustomHeaderMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(APPS_DIR / 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(APPS_DIR / "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': getenv("POSTGRES_DB"),
-        'USER': getenv("POSTGRES_USER"),
-        'PASSWORD': getenv("POSTGRES_PASSWORD"),
-        'PORT': getenv("POSTGRES_PORT"),
-        'HOST': getenv("POSTGRES_HOST"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "PORT": getenv("POSTGRES_PORT"),
+        "HOST": getenv("POSTGRES_HOST"),
     }
 }
 
@@ -110,16 +109,16 @@ PASSWORD_HASHERS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -127,9 +126,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -141,14 +140,14 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'user_auth.User'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "user_auth.User"
 DEFAULT_BIRTH_DATE = date(1900, 1, 1)
 DEFAULT_DATE = date(1900, 1, 1)
 DEFAULT_EXPIRY_DATE = date(2026, 1, 1)
@@ -157,38 +156,77 @@ DEFAULT_PHONE_NUMBER = "+22997123456"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": ["core_apps.common.cookie_auth.CookieAuthentication"],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
+    "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "50/day",
+        "user": "100/day",
+    },
 }
+
+SIMPLE_JWT = {
+    "SIGNING_KEY": getenv("SIGNING_KEY"),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "id",
+    "LOGIN_FIELD": "email",
+    "TOKEN_MODEL": None,
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "SEND_ACTIVATION_EMAIL": True,
+    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+    "PASSWORD_RESET_CONFIRM_RETYPE": True,
+    "ACTIVATION_URL": "activate/{uid}/{token}",
+    "PASSWORD_RESET_CONFIRM_URL": "password-reset/{uid}/{token}",
+    "SERIALIZERS": {
+        "user_create": "core_apps.user_auth.serializers.UserCreateSerializer",
+    },
+}
+
 SPECTACULAR_SETTINGS = {
     "TITLE": "NextGen Bank API",
     "DESCRIPTION": "An API built for a banking system",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
-    "LICENCE": {
-        "name": "MIT License",
-        "url": "https://opensource.org/licence/mit"
-    }
+    "LICENCE": {"name": "MIT License", "url": "https://opensource.org/licence/mit"},
 }
 
 if USE_TZ:
     CELERY_TIMEZONE = TIME_ZONE
 
-CELERY_BROKER_URL=getenv("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND=getenv("CELERY_RESULT_BACKEND")
-CELERY_ACCEPT_CONTENT=["application/json"]
-CELERY_TASK_SERIALIZER="json"
-CELERY_RESULT_SERIALIZER="json"
-CELERY_RESULT_BACKEND_MAX_RETRIES=10
-CELERY_TASK_SEND_SENT_EVENT=True
-CELERY_RESULT_EXTENDED=True
-CELERY_RESULT_BACKEND_ALWAYS_RETRY=True
-CELERY_TASK_TIME_LIMIT=5*60
-CELERY_TASK_SOFT_TIME_LIMIT=60
-CELERY_BEAT_SCHEDULER="django_celery_beat.schedulers:DatabaseScheduler"
-CELERY_WORKER_SEND_TASK_EVENTS=True
+CELERY_BROKER_URL = getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = getenv("CELERY_RESULT_BACKEND")
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+CELERY_RESULT_EXTENDED = True
+CELERY_RESULT_BACKEND_ALWAYS_RETRY = True
+CELERY_TASK_TIME_LIMIT = 5 * 60
+CELERY_TASK_SOFT_TIME_LIMIT = 60
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CELERY_WORKER_SEND_TASK_EVENTS = True
 
-CLOUDINARY_API_KEY=getenv("CLOUDINARY_API_KEY")
-CLOUDINARY_API_SECRET=getenv("CLOUDINARY_API_SECRET")
-CLOUDINARY_CLOUD_NAME=getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = getenv("CLOUDINARY_API_SECRET")
+CLOUDINARY_CLOUD_NAME = getenv("CLOUDINARY_CLOUD_NAME")
 
 cloudinary.config(
     cloud_name=CLOUDINARY_CLOUD_NAME,
@@ -196,18 +234,24 @@ cloudinary.config(
     api_secret=CLOUDINARY_API_SECRET,
 )
 
+COOKIE_NAME = "access"
+COOKIE_SAMESITE = "Lax"
+COOKIE_PATH = "/"
+COOKIE_HTTPONLY = True
+COOKIE_SECURE = getenv("COOKIE_SECURE", "True") == "True"
+
 LOGGING_CONFIG = None
 LOGURU_LOGGING = {
     "handlers": [
         {
             "sink": BASE_DIR / "logs/debug.log",
             "level": "DEBUG",
-            "filter": lambda record: record['level'].no <= logger.level('WARNING').no,
+            "filter": lambda record: record["level"].no <= logger.level("WARNING").no,
             "format": "{time:YYYY-MM-DD HH:mm;ss.SSS} | {level: <8} | "
             "{name}:{function}:{line} - {message}",
             "rotation": "10MB",
             "retention": "30 days",
-            "compression": "zip"
+            "compression": "zip",
         },
         {
             "sink": BASE_DIR / "logs/error.log",
@@ -218,14 +262,14 @@ LOGURU_LOGGING = {
             "retention": "30 days",
             "compression": "zip",
             "backtrace": True,
-            "diagnose": True
+            "diagnose": True,
         },
     ]
 }
 
 
 logger.configure(**LOGURU_LOGGING)
-LOGGING={
+LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
@@ -234,7 +278,7 @@ LOGGING={
             "root": {
                 "handlers": ["loguru"],
                 "level": "DEBUG",
-            }
+            },
         }
-    }
+    },
 }
